2f55a340a3b9d0da71823243b526596f7130e3f6
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-07-06 11:40:46 +0300

    adding event listeners

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    addingEventListener();
+});
